378c666a755d5e42b74596cf1296f442442324ba
Thaha Vp <thahaponnani@gmail.com>
2022-01-07 23:53:03 +0400

    side bar with css done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/home';
+import Reports from './pages/orders';
+import Products from './pages/orders';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<Reports />}/>
+      <Route path="/reports" element={<Reports />}/>
+    </Routes>
+  </BrowserRouter>
+     
+    </>
   );
 }
 
